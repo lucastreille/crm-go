e2744706d1f6c79c727a868cd79610ecc296e4b3
lucastreille <lucas.treille1.0@gmail.com>
2025-11-03 13:54:39 +0100

    feat: init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucastreille/crm-go
+
+go 1.25.3
---
+++ main_internal_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func Example_main() {
+	main()
+	// Output:
+	// Hello, world
+}
+
+func TestGreet(t *testing.T) {
+
+}
